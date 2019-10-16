e2a57b3c8aed2d5d5c5cf9e2213d2e566d467a7a
gholdman1 <gholdman@protonmail.com>
2019-10-16 10:52:05 -0500

    Remove default talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,25 +1,5 @@
 var addressPoints = [
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192363
-  ],
-  [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728639
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276474
-  ],
-  [
-    "Los Angeles, CA",
-    34.0536909,
-    -118.2427666
-  ],
-  [
     "Boston, MA",
     42.3602534,
     -71.0582912
